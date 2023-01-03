bdfba378f8b6d449ab35cff7d8b6b4577c48fd9c
digest15 <digest15@yandex.ru>
2023-01-03 20:41:39 +0800

    #229175 #376992
    Get state threats

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,13 +3,15 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Runnable printThreadName = () -> System.out.println(Thread.currentThread().getName());
-        var first = new Thread(printThreadName);
-        var second = new Thread(printThreadName);
+        var first = new Thread(printThreadName, "first");
+        var second = new Thread(printThreadName, "second");
         first.start();
         second.start();
-        while (first.getState() == Thread.State.TERMINATED
-                && second.getState() == Thread.State.TERMINATED) {
-            System.out.println("All threads finish his work");
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " : " + first.getState());
+            System.out.println(second.getName() + " : " + second.getState());
         }
+        System.out.println("All threads finish his work");
     }
 }
