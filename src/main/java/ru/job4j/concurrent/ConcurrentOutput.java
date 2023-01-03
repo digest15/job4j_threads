3a3a56f40ad4e86f15828c1d4efe2a1516bc035b
digest15 <digest15@yandex.ru>
2023-01-03 12:46:16 +0800

    #1016
    Start threads

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,12 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Runnable print = () -> System.out.println(Thread.currentThread().getName());
+        Thread another = new Thread(print);
+        Thread second = new Thread(print);
+        another.start();
+        second.start();
+        print.run();
+    }
+}
