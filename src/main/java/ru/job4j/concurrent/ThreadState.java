4e99a5cf8da4a27bc0222074a9c4ca2303be8fac
digest15 <digest15@yandex.ru>
2023-01-03 14:12:17 +0800

    #229175
    Threads state

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Runnable printThreadName = () -> System.out.println(Thread.currentThread().getName());
+        var first = new Thread(printThreadName);
+        var second = new Thread(printThreadName);
+        first.start();
+        second.start();
+        while (true) {
+            if (Thread.State.TERMINATED.equals(first.getState())
+                    && Thread.State.TERMINATED.equals(second.getState())) {
+                System.out.println("All threads finish his work");
+                break;
+            }
+        }
+    }
+}
