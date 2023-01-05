5caee6129c85149ff586a3ebf1154803cc4f4b85
digest15 <digest15@yandex.ru>
2023-01-05 13:29:50 +0800

    #267918
    Immutable Note class

--- src/main/java/ru/job4j/concurrent/Node.java
+++ src/main/java/ru/job4j/concurrent/Node.java
@@ -1,22 +1,19 @@
 package ru.job4j.concurrent;
 
 public class Node<T> {
-    private Node<T> next;
-    private T value;
+    private final Node<T> next;
+    private final T value;
+
+    public Node(final Node<T> next, final T value) {
+        this.next = next;
+        this.value = value;
+    }
 
     public Node<T> getNext() {
         return next;
     }
 
-    public void setNext(Node<T> next) {
-        this.next = next;
-    }
-
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 }
