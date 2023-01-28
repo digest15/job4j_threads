ecc91d8e5533dcd916167eae158a7d5ba45b2ee9
digest15 <digest15@yandex.ru>
2023-01-28 12:26:04 +0800

    #315067 #385469
    Remove inner class

--- src/main/java/ru/job4j/concurrent/ParallelIndexSearcher.java
+++ src/main/java/ru/job4j/concurrent/ParallelIndexSearcher.java
@@ -1,67 +1,53 @@
 package ru.job4j.concurrent;
 
-import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelIndexSearcher<T> {
+public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
 
-    private final ForkJoinPool pool;
+    private static final int THRESHOLD = 10;
 
     private final T[] array;
+    private final int from;
+    private final int to;
+    private final T value;
 
-    private volatile boolean isFound;
-
-    public ParallelIndexSearcher(ForkJoinPool pool, T[] array) {
-        this.pool = pool;
+    public ParallelIndexSearcher(T[] array, int from, int to, T value) {
         this.array = array;
+        this.from = from;
+        this.to = to;
+        this.value = value;
     }
 
-    public int indexOf(T value) {
-        isFound = false;
-        return pool.invoke(new SearchTask(0, array.length, value));
+    public static <T> int indexOf(T[] array, T value) {
+        ForkJoinPool pool = new ForkJoinPool();
+        return pool.invoke(new ParallelIndexSearcher<>(array, 0, array.length, value));
     }
 
-    private class SearchTask extends RecursiveTask<Integer> {
-        private static final int THRESHOLD = 10;
-
-        private final int from;
-        private final int to;
-        private final T value;
-
-        public SearchTask(int from, int to, T value) {
-            this.from = from;
-            this.to = to;
-            this.value = value;
+    @Override
+    protected Integer compute() {
+        if (to - from <= THRESHOLD) {
+            return lineSearch();
         }
+        int border = (to - from) / 2 + from;
+        ParallelIndexSearcher<T> leftTask = new ParallelIndexSearcher<>(array, from, border, value);
+        ParallelIndexSearcher<T> rightTask = new ParallelIndexSearcher<>(array, border, to, value);
+        leftTask.fork();
+        rightTask.fork();
+        int li = leftTask.join();
+        int ri = rightTask.join();
 
-        @Override
-        protected Integer compute() {
-            if (to - from <= THRESHOLD) {
-                return lineSearch();
+        return Math.max(li, ri);
+    }
+
+    private Integer lineSearch() {
+        int result = -1;
+        for (int i = from; i < to; i++) {
+            if (value.equals(array[i])) {
+                result = i;
+                break;
             }
-            int border = (to - from) / 2 + from;
-            SearchTask leftTask = new SearchTask(from, border,  value);
-            SearchTask rightTask = new SearchTask(border, to, value);
-            leftTask.fork();
-            rightTask.fork();
-            int li = leftTask.join();
-            int ri = rightTask.join();
-
-            return Math.max(li, ri);
         }
-
-        private Integer lineSearch() {
-            int result = -1;
-            for (int i = from; i < to && !isFound; i++) {
-                if (value.equals(array[i])) {
-                    result = i;
-                    isFound = true;
-                    break;
-                }
-            }
-            return result;
-        }
+        return result;
     }
-
 }
--- src/test/java/ru/job4j/concurrent/ParallelIndexSearcherTest.java
+++ src/test/java/ru/job4j/concurrent/ParallelIndexSearcherTest.java
@@ -2,18 +2,16 @@ package ru.job4j.concurrent;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
-class ParallelIndexSearchTest {
+class ParallelIndexSearcherTest {
     @Test
     public void whenSearchInSmallArray() {
         int expectedIndex = 4;
         Integer[] array = IntStream.range(0, 8).boxed().toArray(Integer[]::new);
-        ParallelIndexSearcher<Integer> indexSearcher = new ParallelIndexSearcher<>(new ForkJoinPool(), array);
-        int index = indexSearcher.indexOf(expectedIndex);
+        int index = ParallelIndexSearcher.indexOf(array, expectedIndex);
         assertThat(index).isEqualTo(expectedIndex);
     }
 
@@ -21,8 +19,7 @@ class ParallelIndexSearchTest {
     public void whenSearchInHugeArray() {
         int expectedIndex = 50001;
         Integer[] array = IntStream.range(0, 100000).boxed().toArray(Integer[]::new);
-        ParallelIndexSearcher<Integer> indexSearcher = new ParallelIndexSearcher<>(new ForkJoinPool(), array);
-        int index = indexSearcher.indexOf(expectedIndex);
+        int index = ParallelIndexSearcher.indexOf(array, expectedIndex);
         assertThat(index).isEqualTo(expectedIndex);
     }
 
@@ -30,8 +27,7 @@ class ParallelIndexSearchTest {
     public void whenSearchAbsentValue() {
         int expectedIndex = -1;
         Integer[] array = IntStream.range(0, 8).boxed().toArray(Integer[]::new);
-        ParallelIndexSearcher<Integer> indexSearcher = new ParallelIndexSearcher<>(new ForkJoinPool(), array);
-        int index = indexSearcher.indexOf(expectedIndex);
+        int index = ParallelIndexSearcher.indexOf(array, expectedIndex);
         assertThat(index).isEqualTo(expectedIndex);
     }
 
@@ -39,8 +35,7 @@ class ParallelIndexSearchTest {
     public void whenSearchInStringArray() {
         int expectedIndex = 3;
         String[] array = IntStream.range(0, 8).mapToObj(String::valueOf).toArray(String[]::new);
-        ParallelIndexSearcher<String> indexSearcher = new ParallelIndexSearcher<>(new ForkJoinPool(), array);
-        int index = indexSearcher.indexOf(String.valueOf(expectedIndex));
+        int index = ParallelIndexSearcher.indexOf(array, String.valueOf(expectedIndex));
         assertThat(index).isEqualTo(expectedIndex);
     }
 }
