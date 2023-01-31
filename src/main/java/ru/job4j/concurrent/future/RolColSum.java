f888c6b9bf8f6b1be3416521df23b2585129857c
digest15 <digest15@yandex.ru>
2023-01-31 12:02:14 +0800

    #361626
    Synchronous and Asynchronous implementations counter summing rows and columns of matrix.

---
+++ src/main/java/ru/job4j/concurrent/future/RolColSum.java
@@ -0,0 +1,55 @@
+package ru.job4j.concurrent.future;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class RolColSum {
+    public record Sums(int rowSum, int colSum){}
+
+    public static Sums[] sum(int[][] matrix) {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            int k = i;
+            sums[i] = new Sums(
+                    sum(matrix, j -> j, j -> k),
+                    sum(matrix, j -> k, j -> j)
+            );
+        }
+        return sums;
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        Map<Integer, CompletableFuture<Sums>> futureMap = new HashMap<>();
+        for (int i = 0; i < matrix.length; i++) {
+            int k = i;
+            futureMap.put(
+                    i,
+                    CompletableFuture.supplyAsync(() -> sum(matrix, j -> j, j -> k))
+                            .thenCombine(
+                                    CompletableFuture.supplyAsync(() -> sum(matrix, j -> k, j -> j)),
+                                    Sums::new
+                            )
+            );
+        }
+        Sums[] sums = new Sums[matrix.length];
+        for (Integer key : futureMap.keySet()) {
+            sums[key] = futureMap.get(key).get();
+        }
+        return sums;
+    }
+
+    private static int sum(int[][] matrix, IntToIntFunction row, IntToIntFunction col) {
+        int sum = 0;
+        for (int j = 0; j < matrix.length; j++) {
+            sum += matrix[col.apply(j)][row.apply(j)];
+        }
+        return sum;
+    }
+
+    @FunctionalInterface
+    private interface IntToIntFunction {
+        int apply(int value);
+    }
+}
---
+++ src/test/java/ru/job4j/concurrent/future/RolColSumTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.concurrent.future;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RolColSumTest {
+
+    private static final int SIZE = 50000;
+
+    private static int[][] matrics;
+
+    @BeforeAll
+    public static void initMatrix() {
+        matrics = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                matrics[i][j] = j;
+            }
+        }
+    }
+
+    @Test
+    public void whenCountSum() {
+        long start = System.currentTimeMillis();
+        RolColSum.Sums[] sums = RolColSum.sum(matrics);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+    @Test
+    public void whenCountSumAsync() throws Exception {
+        long start = System.currentTimeMillis();
+        RolColSum.Sums[] sums = RolColSum.asyncSum(matrics);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
